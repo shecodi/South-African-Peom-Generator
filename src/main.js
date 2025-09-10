3fd26e281ed988ff5112147762b2629b65ee181b
God'swill Kutwane <gkutwane7@gmail.com>
2025-09-10 14:24:19 +0200

    Added the AI API integration

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
+function displayPoem (response){
+
+    new Typewriter("#poem", {
+      strings:response.data.answer,
+      autoStart: true,
+      delay: 2,
+      cursor: "",
+    });
+
+}
+
 
 function generatePoem (event) {
 
   event.preventDefault () ;
 
-  new Typewriter ("#poem", {
-    strings : "The beauty lies in the eyes ",
-    autoStart : true ,
-    delay : 2 ,
-    cursor : "" ,
-  });
+let instructionsInput = document.querySelector("#user-instructions");
+let apikey = "5fa91afbb646o4d4c1d2at71e7c05a3b";
+let prompt = `User instructions : Generate a South African poem about ${instructionsInput.value}`;
+let context = "You're a romantic Poem expect and love to write  short poems. Your mission is to generate a 4 line poem , provided in HTML format. example : <div>This is the poem <div/> and seperate each line with a <br/>. Make sure to follow the user instructions. Don't include a tittle to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the bottom of the poem. ";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+axios.get(apiUrl).then(displayPoem);
+
+
 }
 
 
