119eebbd359e761cb6c9f15f9d469006ae5c4170
God'swill Kutwane <gkutwane7@gmail.com>
2025-09-11 10:51:08 +0200

    Just added the background colour and added more colour

--- src/main.js
+++ src/main.js
@@ -16,14 +16,14 @@ function generatePoem (event) {
 
 let instructionsInput = document.querySelector("#user-instructions");
 let apikey = "5fa91afbb646o4d4c1d2at71e7c05a3b";
-let prompt = `User instructions : Generate a South African poem about ${instructionsInput.value}`;
-let context = "You're a romantic Poem expect and love to write  short poems. Your mission is to generate a 4 line poem , provided in HTML format. example : <div>This is the poem <div/> and seperate each line with a <br/>. Make sure to follow the user instructions. Don't include a tittle to the poem. Sign the poem with `SheCodes AI` inside a <strong> element at the bottom of the poem. ";
+let prompt = `User instructions : Generate a African poem about ${instructionsInput.value}`;
+let context = "You're a romantic Poem expect and love to write  short poems. Your mission is to generate a 8 line poem , provided in HTML format. example : <div>This is the poem <div/> and seperate each line with a <br/>. Make sure to follow the user instructions. Don't include a tittle to the poem. Sign the poem with `Abijah God'swill Kutwane` inside a <strong> element at the bottom of the poem. ";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
 
 
  let poemElement = document.querySelector("#poem");
  poemElement.classList.remove("hidden");
- poemElement.innerHTML = `<div class="generating">⏳ Generating a South African poem about ${instructionsInput.value}</div>`;
+ poemElement.innerHTML = `<div class="generating">⏳ Generating an African poem about ${instructionsInput.value}</div>`;
 
 
 
